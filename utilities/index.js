a639645b81ad8fed2e1d83923036eedd8b951b52
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-10-13 00:38:47 -0600

    Added function to check for authentication

--- utilities/index.js
+++ utilities/index.js
@@ -4,4 +4,11 @@ const utilities = {};
 utilities.errorHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
+utilities.isAuthenticated = (req, res, next) => {
+  if (req.session.user === undefined) {
+    return res.status(401).json('Please login to view this page.');
+  }
+  next();
+};
+
 module.exports = utilities;
