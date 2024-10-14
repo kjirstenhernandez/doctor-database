3ca001fa1583951ddaf8e14eaf5e8e045dda4764
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-10-13 19:39:41 -0600

    encapsulated passport in it's own function

--- utilities/passport-setup.js
+++ utilities/passport-setup.js
@@ -3,44 +3,48 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const dotenv = require('dotenv').config();
 const users = require('../controllers/users');
 
-passport.serializeUser((user, done) => {
-  console.log('Serializing user:', user);
-  console.log('Google Id: ' + user.googleId);
-  done(null, user.googleId);
-});
-
-passport.deserializeUser((id, done) => {
-  const timestamp = Date.now();
-  const date = new Date(timestamp);
-  const time = date.toLocaleTimeString();
-  users.getUserById(id).then((user) => {
-    console.log('Deserialize user: ', user);
-    console.log('Time: ', time);
-    done(null, user);
+const initializePassport = () => {
+  passport.serializeUser((user, done) => {
+    console.log('Serializing user:', user);
+    console.log('Google Id: ' + user.googleId);
+    done(null, user.googleId);
   });
-});
 
-passport.use(
-  new GoogleStrategy(
-    {
-      // options for google strategy
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: '/auth/google/redirect' // had this programmed into .env but testing to see if it makes the difference for render?
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        const currentUser = await users.getUser(profile);
+  passport.deserializeUser((id, done) => {
+    const timestamp = Date.now();
+    const date = new Date(timestamp);
+    const time = date.toLocaleTimeString();
+    users.getUserById(id).then((user) => {
+      console.log('Deserialize user: ', user);
+      console.log('Time: ', time);
+      done(null, user);
+    });
+  });
 
-        if (currentUser != null) {
-          done(null, currentUser);
-        } else {
-          const newUser = await users.createUser(profile);
-          done(null, newUser);
+  passport.use(
+    new GoogleStrategy(
+      {
+        // options for google strategy
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: '/auth/google/redirect' // had this programmed into .env but testing to see if it makes the difference for render?
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const currentUser = await users.getUser(profile);
+
+          if (currentUser != null) {
+            done(null, currentUser);
+          } else {
+            const newUser = await users.createUser(profile);
+            done(null, newUser);
+          }
+        } catch (err) {
+          done(err, null);
         }
-      } catch (err) {
-        done(err, null);
       }
-    }
-  )
-);
+    )
+  );
+};
+
+module.exports = initializePassport;
